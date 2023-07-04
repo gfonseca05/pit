2071411bb729eaa60e556b9c85b69eee627ea2cd
Lucas-Alves-S <lucasalvessouzalima05@gmail.com>
2023-07-04 15:12:16 -0300

    Correção alterar senha

--- Hub/Profile/profile.php
+++ Hub/Profile/profile.php
@@ -131,7 +131,7 @@ if ($quant['QUANTIDADE'] == 1) {
                 <a href="update.php"
                     class="edit rounded-lg bg-blue-400 transition duration-700 ease-in-out hover:bg-blue-600 w-full row-span-2 flex items-center justify-center md:row-start-5 md:row-span-2 md:col-span-1 md:h-full md:bg-blue-800 md:hover:bg-indigo-600 py-3">Editar
                     Perfil</a>
-                <a href="../../database/password.php"
+                <a href="../password.php"
                     class="changePsw rounded-lg bg-blue-500 transition duration-700 ease-in-out hover:bg-blue-600 w-full row-span-2 flex items-center justify-center md:row-start-5 md:row-span-2 md:col-span-1 md:h-full md:bg-blue-800 md:hover:bg-indigo-700 py-3">
                     Alterar Senha</a>
                 <a href="../../Login/login.php"
--- Hub/password.php
+++ Hub/password.php
@@ -1,10 +1,10 @@
 <?php
-require("PHPMailer/mail.php");
-require("config.php");
+require_once("../database/config.php");
+require_once("../database/PHPMailer/mail.php");
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-  header("Location: login.php");
+if (!isset($_SESSION['email'])) {
+  header("Location: ../Login/login.php");
   exit();
 }
 
@@ -28,7 +28,7 @@ sendEmail($row['email'], $row['nome'], $row['verify_cod']);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+  <link href="../dist/output.css" rel="stylesheet">
 </head>
 
 <body>
@@ -44,10 +44,10 @@ sendEmail($row['email'], $row['nome'], $row['verify_cod']);
       d="M0,48L30,40C60,32,120,16,180,22C240,28,300,56,360,60C420,64,480,44,540,46C600,48,660,72,720,80C780,88,840,80,900,80C960,80,1020,88,1080,80C1140,72,1200,48,1260,42C1320,36,1380,48,1440,60C1500,72,1560,84,1620,74C1680,64,1740,32,1800,18C1860,4,1920,8,1980,16C2040,24,2100,36,2160,42C2220,48,2280,48,2340,42C2400,36,2460,24,2520,22C2580,20,2640,28,2700,38C2760,48,2820,60,2880,56C2940,52,3000,32,3060,32C3120,32,3180,52,3240,60C3300,68,3360,64,3420,54C3480,44,3540,28,3600,24C3660,20,3720,28,3780,44C3840,60,3900,84,3960,86C4020,88,4080,68,4140,62C4200,56,4260,64,4290,68L4320,72L4320,120L4290,120C4260,120,4200,120,4140,120C4080,120,4020,120,3960,120C3900,120,3840,120,3780,120C3720,120,3660,120,3600,120C3540,120,3480,120,3420,120C3360,120,3300,120,3240,120C3180,120,3120,120,3060,120C3000,120,2940,120,2880,120C2820,120,2760,120,2700,120C2640,120,2580,120,2520,120C2460,120,2400,120,2340,120C2280,120,2220,120,2160,120C2100,120,2040,120,1980,120C1920,120,1860,120,1800,120C1740,120,1680,120,1620,120C1560,120,1500,120,1440,120C1380,120,1320,120,1260,120C1200,120,1140,120,1080,120C1020,120,960,120,900,120C840,120,780,120,720,120C660,120,600,120,540,120C480,120,420,120,360,120C300,120,240,120,180,120C120,120,60,120,30,120L0,120Z">
     </path>
   </svg>
-  <form action="database.php" method="POST" class="container min-w-full min-h-screen grid">
+  <form action="../database/database.php" method="POST" class="container min-w-full min-h-screen grid">
     <input type="hiden" name="acao" value="esquecisenha" class="hidden">
     <div
-      class="login p-4 h-96 w-80 self-center justify-self-center shadow-lg shadow-gray-400/40 rounded-lg grid grid-cols-1 gap-3 grid-rows-4 gap-4">
+      class="login p-4 h-96 w-80 self-center justify-self-center shadow-lg shadow-gray-400/40 rounded-lg grid grid-cols-1 gap-3 grid-rows-4">
       <div class="Code">
         <label for="code">Código recebido por email</label> <br />
         <div class="relative">
@@ -64,7 +64,7 @@ sendEmail($row['email'], $row['nome'], $row['verify_cod']);
             type="password" name="psw"  required />
           <i class="absolute top-0 right-0 h-full w-5 flex items-center justify-center mr-4 cursor-pointer"
             onclick="viewPSW(document.getElementById('senha'), document.getElementById('eye'))">
-            <img id="eye" src="../assets/eye-closed-svgrepo-com.svg"></img>
+            <img id="eye" src="../../assets/eye-closed-svgrepo-com.svg"></img>
           </i>
         </div>
       </div>
@@ -76,7 +76,7 @@ sendEmail($row['email'], $row['nome'], $row['verify_cod']);
             type="password" name="Rpsw" oninput="password()" required />
           <i class="absolute top-0 right-0 h-full w-5 flex items-center justify-center mr-4 cursor-pointer"
             onclick="viewPSW(document.getElementById('repetSenha'), document.getElementById('Reye'))">
-            <img id="Reye" src="../assets/eye-closed-svgrepo-com.svg"></img>
+            <img id="Reye" src="../../assets/eye-closed-svgrepo-com.svg"></img>
           </i>
         </div>
       </div>
@@ -99,7 +99,7 @@ sendEmail($row['email'], $row['nome'], $row['verify_cod']);
       d="M0,48L30,40C60,32,120,16,180,22C240,28,300,56,360,60C420,64,480,44,540,46C600,48,660,72,720,80C780,88,840,80,900,80C960,80,1020,88,1080,80C1140,72,1200,48,1260,42C1320,36,1380,48,1440,60C1500,72,1560,84,1620,74C1680,64,1740,32,1800,18C1860,4,1920,8,1980,16C2040,24,2100,36,2160,42C2220,48,2280,48,2340,42C2400,36,2460,24,2520,22C2580,20,2640,28,2700,38C2760,48,2820,60,2880,56C2940,52,3000,32,3060,32C3120,32,3180,52,3240,60C3300,68,3360,64,3420,54C3480,44,3540,28,3600,24C3660,20,3720,28,3780,44C3840,60,3900,84,3960,86C4020,88,4080,68,4140,62C4200,56,4260,64,4290,68L4320,72L4320,120L4290,120C4260,120,4200,120,4140,120C4080,120,4020,120,3960,120C3900,120,3840,120,3780,120C3720,120,3660,120,3600,120C3540,120,3480,120,3420,120C3360,120,3300,120,3240,120C3180,120,3120,120,3060,120C3000,120,2940,120,2880,120C2820,120,2760,120,2700,120C2640,120,2580,120,2520,120C2460,120,2400,120,2340,120C2280,120,2220,120,2160,120C2100,120,2040,120,1980,120C1920,120,1860,120,1800,120C1740,120,1680,120,1620,120C1560,120,1500,120,1440,120C1380,120,1320,120,1260,120C1200,120,1140,120,1080,120C1020,120,960,120,900,120C840,120,780,120,720,120C660,120,600,120,540,120C480,120,420,120,360,120C300,120,240,120,180,120C120,120,60,120,30,120L0,120Z">
     </path>
   </svg>
-  <script src="script.js"></script>
+  <script src="../Cadastro/script.js"></script>
 
 
 </body>
